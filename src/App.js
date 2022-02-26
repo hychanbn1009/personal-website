1f1376ef6d445469a3e489deea92996e28194c09
fishball <hychanbn1009@gmail.com>
2022-02-26 23:31:38 +0800

    update redirect url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Sidebar from './component/Sidebar/Sidebar';
 import Homepage from './component/Homepage/Homepage';
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route,Navigate } from 'react-router-dom';
 import Projects from './component/Projects/Projects';
 import Skills from './component/Skills/Skills';
 import About from './component/About/About';
@@ -13,6 +13,7 @@ function App() {
       <Sidebar/>
       <div className='content'>
         <Routes>
+            <Route path='/personal-website' element={<Navigate to="/"/>}/>
             <Route path='/' element={<Homepage/>}/>
             <Route path='/about' element={<About/>}/>
             <Route path='/skills' element={<Skills/>}/>
