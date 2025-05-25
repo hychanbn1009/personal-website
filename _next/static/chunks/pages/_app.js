f3929b7144b144f3c8f484cf8f85cff9c43f02d1
hychanbn1009 <hychanbn1009@gmail.com>
2025-05-25 20:08:50 +0800

    Updates

--- _next/static/chunks/pages/_app.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/60ae9f2e48d78dbb.js",
-  "static/chunks/8924d143e4ae40dd.js",
-  "static/chunks/eb71396898665327.js"
-])
--- _next/static/chunks/pages/_error.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/3c85ae08889d1d13.js",
-  "static/chunks/8924d143e4ae40dd.js",
-  "static/chunks/79f0db29dbc553c8.js"
-])
